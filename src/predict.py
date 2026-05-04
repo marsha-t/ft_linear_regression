dd601a3e40d3a340dfee2f609749d21728ff316b
Marsha Teo <marsha.teo@gmail.com>
2026-05-04 09:57:42 +0400

    finish predict.py

--- src/predict.py
+++ src/predict.py
@@ -1,11 +1,11 @@
 # Script to run prediction
 theta0 = 0 # TODO Temp var
-theta1 = 0.01 # TODO Temp var
+theta1 = 0 # TODO Temp var
 
 def validate_mileage(mileage):
 	try: 
 		mileage = float(mileage)
-	except:
+	except ValueError:
 		return None, "Please input numbers only"
 	
 	if (mileage < 0):
@@ -13,14 +13,17 @@ def validate_mileage(mileage):
 	return mileage, None
 
 
-def estimate_price(mileage):
+def estimate_price(mileage, theta0, theta1):
 	return theta0 + theta1 * mileage
 
-user_input = input("Input mileage: ")
-mileage, error = validate_mileage(user_input)
-if error:
-	print(error)
-else :
-	print(f"Mileage: {mileage}")
-	print(f"Predicted price: {estimate_price(mileage)}")
+while True:
+	user_input = input("Input mileage: ")
+	mileage, error = validate_mileage(user_input)
+	if error:
+		print(error + "\n")
+	else :
+		print(f"Mileage: {mileage:,.2f}")
+		print(f"Predicted price: {estimate_price(mileage, theta0, theta1):,.2f}")
+		print("Note: Figures are rounded to 2 decimal points")
+		break
 
