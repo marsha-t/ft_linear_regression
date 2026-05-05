aa1b25539e3283f2dc4e9eb526d13db051618b1d
Marsha Teo <marsha.teo@gmail.com>
2026-05-05 09:55:41 +0400

    add tests and restructure as packages

---
+++ tests/test_model.py
@@ -0,0 +1,10 @@
+from src.model import LinearRegressionModel
+
+model = LinearRegressionModel()
+x = [1, 2, 3]
+y = [2, 4, 6]
+
+model.fit(x, y, 0.1, 20000)
+
+print(model.theta0, model.theta1)
+print(model.predict(4))
